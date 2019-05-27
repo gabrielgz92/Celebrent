b10d32ea4e5a4896cecd3d20a247c96f7afd2932
tom conway <thomconway94@gmail.com>
2019-05-27 17:16:33 +1000

    fixed validations issues

--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -1,4 +1,5 @@
 class Celebrity < ApplicationRecord
-  validates :first_name, :last_name, presence: true, uniqueness: true
+  validates :first_name, presence: true, uniqueness: true
+  validates :last_name, presence: true, uniqueness: true
   validates :photo, :description, :rate_per_hour, :date_of_birth, :gender, presence: true
 end
