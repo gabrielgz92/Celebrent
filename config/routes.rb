7aeb3a3f125f8f98b514e090b4c69dd43446e276
GabrielGZ <ggonzalezzabala@gmail.com>
2019-05-28 09:57:56 +1000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'celebrities#index'
+
+ resources :celebrities, only: [:index, :show] do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:index, :edit, :update]
+
+  get '/my-profile', to: 'user#profile'
+  get 'edit-profile/:id/form', to: 'user#edit'
+  patch 'edit-profile/:id', to: 'user#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
