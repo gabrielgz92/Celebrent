03fe249afa5d129dfac46149d04264ff2f2eae68
GabrielGZ <ggonzalezzabala@gmail.com>
2019-05-27 16:57:14 +1000

    added missing fields to user

---
+++ db/migrate/20190527065604_add_fields_to_user.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :date_of_birth, :datetime
+    add_column :users, :description, :text
+    add_column :users, :gender, :string
+    add_column :users, :photo, :string
+  end
+end
