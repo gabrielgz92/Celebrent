5e1772dbea494944a53122b4cc5e927ea9183fc5
Krisztina Wiseman <krisztinawiseman@aol.com>
2019-05-27 17:47:59 +1000

    created validation for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :celebrity
+  BOOKING_STATUS = ['accepted', 'pending', 'cancelled', 'completed']
+  validates :status inclusion: { in: BOOKING_STATUS }
+  validates :status, :duration, :rate_per_hour, presence: true
+  validates rate_per_hour, numericaltity: { only_integer: true }
+  validates rate_per_hour, numericaltity: { greater_than_or_equal_to: 1 }
 end
