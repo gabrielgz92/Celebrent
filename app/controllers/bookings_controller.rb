7e6963866f666a9058d9566abb7809e6d2e2ab55
GabrielGZ <ggonzalezzabala@gmail.com>
2019-05-28 14:37:41 +1000

    booking actions new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,21 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:edit, :update]
+  # before_action :set_booking, only: [:edit, :update]
+  before_action(:set_celebrity, except: :destroy)
 
+  def new
+    @booking = Booking.new
+    render 'celebrities/:id'
+  end
 
+  def create
+    @booking = Booking.new
+    @booking.celebrity = @celebrity
+    if @booking.save
+      redirect_to @booking.celebrity
+    else
+      render :new
+    end
+  end
 
   def edit
   end
@@ -19,7 +33,11 @@ class BookingsController < ApplicationController
     params.require(:booking).permit(:status, :duration, :rate_per_hour, :user_id, :celebrity_id)
   end
 
-  def set_booking
-    @booking = Booking.find(params[:id])
+  # def set_booking
+  #   @booking = Booking.find(params[:id])
+  # end
+
+  def set_celebrity
+    @celebrity = Celebrity.find(params[:celebrity_id])
   end
 end
