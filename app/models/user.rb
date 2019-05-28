ed5b303a83bb52a1af3d3e89c9ca6bf14b792543
sophia wu <sophia6734wu@gmail.com>
2019-05-28 18:31:28 +1000

    added uploader for user edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   has_many :bookings, dependent: :destroy
   has_many :celebrities, through: :bookings
   # Include default devise modules. Others available are:
